f3e15891b2a25d8e6263991f627807dbb39559d5
jiwow-wow <jiwow-wow@example.com>
2025-05-12 18:18:01 +0900

    Update src files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import {Routes, Route } from 'react-router-dom';
-
-import Login from './pages/Login/Login';
-import Mainpage from './pages/Mainpage/Mainpage';
-import Userform from './pages/Userform/UserInfoForm'
-
-import './styles/App.css';
-
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import InfoInput from "./pages/InfoInput";
+import Upload from "./pages/Upload";
+import Result from "./pages/Result";
+import Home from "./pages/Home"
+import "./styles/theme.css";
 
 function App() {
-  
-  return (
-    <>
-      <Routes>
-        <Route path = "/" element = {<Mainpage/>}/>
-        <Route path = "/login" element ={<Login/>}/>
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/upload" element={<Upload />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/info" element={<InfoInput />} />  
+                    <Route path="/result" element={<Result />} />
+                </Routes>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,20 @@
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './styles/global.css'; // 글로벌 스타일
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+    document.getElementById('root') as HTMLElement
+    );
 
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
-
-// import reportWebVitals frosrc/reportWebVitalsals'; //이 두 줄은 웹 페이지의 성능 데이터를 측정해서 기록하거나 전송할 수 있게 해주는 함수임
-// reportWebVitals(console.log);
